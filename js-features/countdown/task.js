c1dc99e55bc5588a968e5a458ccb5bd3ca0574d2
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-11-01 23:05:00 +0300

    Доработка замечаний

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -3,14 +3,14 @@ const getTimer = function() {
     timer.textContent -= 1;
     let count = parseInt(document.getElementById('timer').innerHTML, 10);
     if (count < 1) {
-        clearInterval(int);
+        clearInterval(interval);
         alert('Вы победили!!!')
     }
     console.log(timer);
     console.log(count);
 }
 
-let int = setInterval(getTimer, 1000);
+let interval = setInterval(getTimer, 1000);
 
 
 
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,6 +3,11 @@ const lost = document.getElementById('lost');
 
 for (let i = 1; i < 10; i++) {
     const mole = document.getElementById(`hole${i}`);
+    const resultForGame = function(text) {
+        alert(text);
+        dead.textContent = '0';
+        lost.textContent = '0';
+    }
     mole.onclick = function() {
         if (mole.className.includes('hole_has-mole')) {
             dead.textContent++;
@@ -10,13 +15,11 @@ for (let i = 1; i < 10; i++) {
             lost.textContent++;
         }
         if (dead.textContent === '10') {
-            alert("Вы победили!");
-            dead.textContent = '0';
-            lost.textContent = '0';
+            resultForGame("Вы победили!");
+
         } else if (lost.textContent === '5') {
-            alert("Вы проигали");
-            dead.textContent = '0';
-            lost.textContent = '0';
+            resultForGame("Вы проигали");
+
         }
     }
 }
