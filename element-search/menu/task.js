c9b335afe684fb8ff9d47a6856417e78c6bde4c8
Nicko Sinnikov <nickosinnikov@outlook.com>
2021-11-16 16:44:49 +0300

    Исправленное menu и доделанный slider

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,9 +3,9 @@ let menuLinkLength = menuLink.length;
 for (let i = 0; i < menuLinkLength; i++) {
     menuLink[i].onclick = function() {
         let menuSub = menuLink[i].nextElementSibling;
-        menuSub.classList.toggle("menu_active");
-        if (menuLink[i].closest('.menu_main')) {
+        if (menuSub !== null) {
+            menuSub.classList.toggle("menu_active");
             return false;
-        };
+        }
     }
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,39 +4,69 @@ let sliderArrowNext = document.querySelector(".slider__arrow_next");
 sliderArrowPrev.onclick = prevSlide;
 sliderArrowNext.onclick = nextSlide;
 
-//
+
 let sliderItem = Array.from(document.getElementsByClassName('slider__item'));
-let sliderItemActive = document.querySelector('slider__item_active');
-
 let sliderItemLength = sliderItem.length;
-let slideIndex = 0;
 
 
+let dotIndex = 0;
+let dot = Array.from(document.querySelectorAll(".slider__dot"));
 
+
+for (let i = 0; i < dot.length; i++) {
+    dot[i].onclick = function() {
+        let dotIndex = dot.indexOf(dot[i]);
+
+        let dotItemActive = document.querySelector(".slider__dot_active");
+        dotItemActive.classList.remove("slider__dot_active");
+
+        let sliderItemActive = document.querySelector('.slider__item_active');
+        sliderItemActive.className = "slider__item";
+
+        sliderItem[dotIndex].classList.add("slider__item_active");
+        dot[dotIndex].classList.add('slider__dot_active')
+    };
+};
+
+
+let slideIndex = sliderItem.findIndex((arr) => arr.className.includes('slider__item_active'));
+dot[slideIndex].classList.add('slider__dot_active'); // делаем активную точку в соответствии с первым сдайдом
 
 function nextSlide() {
-    for (let i = 0; i < sliderItemLength; i++) {
-        sliderItem[i].classList.remove('slider__item_active');
-    }
+    let sliderItemActive = document.querySelector('.slider__item_active'); // находим активный элемент слайда
+    let dotItemActive = document.querySelector(".slider__dot_active"); // находим активный элемент точки
 
-    if (slideIndex > sliderItemLength) {
+    slideIndex = sliderItem.findIndex((arr) => arr.className.includes('slider__item_active')); // находим индекс активного элемента в массиве
+
+
+    if (slideIndex === (sliderItemLength - 1)) {
         slideIndex = 0;
     } else { slideIndex += 1 }
 
+    sliderItemActive.className = "slider__item";
+    dotItemActive.classList.remove("slider__dot_active");
 
-    sliderItem[slideIndex + 1].classList.add("slider__item_active");
-    console.log(slideIndex);
+    sliderItem[slideIndex].classList.add("slider__item_active");
+    dot[slideIndex].classList.add('slider__dot_active')
+
 }
 
 function prevSlide() {
-    for (let i = 0; i < sliderItemLength; i++) {
-        sliderItem[i].classList.remove('slider__item_active');
-    }
 
-    slideIndex -= 1;
-    if (slideIndex < 0) {
-        slideIndex = sliderItemLength;
-    }
+    let sliderItemActive = document.querySelector('.slider__item_active'); // находим активный элемент
+    let dotItemActive = document.querySelector(".slider__dot_active")
 
-    sliderItem[slideIndex - 1].classList.add("slider__item_active");
+    slideIndex = sliderItem.findIndex((arr) => arr.className.includes('slider__item_active')); // находим индекс активного элемента в массиве
+
+    // проверяем, если элемент самый первый, то след активный элемент будет последним
+    if (slideIndex === 0) {
+        slideIndex = sliderItemLength - 1;
+    } else { slideIndex -= 1 }
+
+    sliderItemActive.className = "slider__item";
+    dotItemActive.classList.remove("slider__dot_active");
+
+    sliderItem[slideIndex].classList.add("slider__item_active");
+    dot[slideIndex].classList.add('slider__dot_active')
+
 }
