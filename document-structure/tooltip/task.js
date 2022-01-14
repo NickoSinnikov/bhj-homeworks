92ec51c91b9736e4d8b79164ddfd4145463d07c7
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-01-14 21:34:42 +0300

    Доработанное tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,24 +1,37 @@
 const hasToolTip = Array.from(document.querySelectorAll(".has-tooltip"));
-
 let positionLink = 0;
-for (let i = 0; i < hasToolTip.length; i++) {
-    hasToolTip[i].onclick = function() {
-        const tooltipActive = document.querySelector(".tooltip_active");
-        const toolTip = document.querySelector(".tooltip");
-        console.log(toolTip);
-        if (tooltipActive !== null) {
-            tooltipActive.classList.remove("tooltip_active");
-        } else {
+let currentElem = "";
 
-            const title = this.getAttribute('title');
-            positionLink = hasToolTip[i].getBoundingClientRect();
-            console.log(positionLink.x + ';' + positionLink.y);
-            hasToolTip[i].insertAdjacentHTML("afterend", `<div class='tooltip tooltip_active' style="left: ${positionLink.x}px ; top: ${positionLink.y + 20}px">${title}</div>`);
+
+const addTooltip = function(event) {
+    event.preventDefault();
+
+    const toolTip = document.querySelector(".tooltip");
+    const title = this.getAttribute('title');
+    let positionX = positionLink.x;
+    let positionY = positionLink.y;
+
+    positionLink = event.currentTarget.getBoundingClientRect();
+
+    if (toolTip !== null) {
+        if (currentElem === event.currentTarget) {
+            toolTip.classList.remove("tooltip_active");
+            currentElem = "";
+        } else {
+            toolTip.classList.add("tooltip_active")
+            toolTip.setAttribute('style', `left: ${positionX}px; top: ${positionY + 20}px `);
+            toolTip.innerText = `${title}`;
+            currentElem = event.currentTarget;
         }
-        return false;
+    } else {
+        event.currentTarget.insertAdjacentHTML("afterend", `<div class='tooltip tooltip_active' style="left: ${positionX}px ; top: ${positionY + 20}px">${title}</div>`);
+        currentElem = event.currentTarget;
     }
+
+    return false;
 }
 
+hasToolTip.forEach(item => item.addEventListener('click', addTooltip));
 
 
 
